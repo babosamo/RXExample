a67e8c02d27eac67676fa782ecfe880a9af1570e
noah <noah.kim@kakaocorp.com>
2016-07-26 17:38:40 +0900

    chapter 1 example

---
+++ app/src/main/java/com/kakao/rxeample/Chatper1.java
@@ -0,0 +1,83 @@
+package com.kakao.rxeample;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+
+/**
+ * Created by Chatper1 on 2016. 7. 26..
+ */
+public class Chatper1 {
+
+    public void doRXTest(final String tag, final TextView textView){
+        Observable<String> simpleObservable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello RxAndroid !!");
+                subscriber.onCompleted();
+            }
+        });
+
+        simpleObservable.subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(tag, "complete!");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(tag, "error: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(String text) {
+                textView.setText(text);
+            }
+        });
+
+        simpleObservable
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String text) {
+                        textView.setText(text);
+                    }
+                });
+
+        simpleObservable
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String text) {
+                        textView.setText(text);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+
+                    }
+                }, new Action0() {
+                    @Override
+                    public void call() {
+
+                    }
+                });
+
+        simpleObservable
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String text) {
+                        textView.setText(text);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+
+                    }
+                });
+
+    }
+
+}
--- app/src/main/java/com/kakao/rxeample/MainActivity.java
+++ app/src/main/java/com/kakao/rxeample/MainActivity.java
@@ -7,39 +7,20 @@ import android.widget.TextView;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Observable<String> simpleObservable = Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("Hello RxAndroid !!");
-                subscriber.onCompleted();
-            }
-        });
-
-        simpleObservable.subscribe(new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-                Log.d(TAG, "complete!");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, "error: " + e.getMessage());
-            }
-
-            @Override
-            public void onNext(String text) {
-                ((TextView) findViewById(R.id.textView)).setText(text);
-            }
-        });
+        textView = ((TextView) findViewById(R.id.textView));
+        new Chatper1().doRXTest(TAG, textView);
     }
 }
